537cf7512935cf7146a8f9ea8c3ab05a1f74c44e
felix mwangi <fmwauramwangi@gmail.com>
2025-02-04 02:35:54 +0300

    src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { format } from "date-fns";
+// import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
 import TestComponent from "./TestComponent";
 
-// Add your code own within the return statement
 function App() {
   return (
     <div className="App">
@@ -16,7 +15,6 @@ function App() {
       </p>
       <ExampleComponent />
       <TestComponent />
-
     </div>
   );
 }
